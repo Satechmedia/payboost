de58070d61ffa1f8e46552bc256ce310fa479b0a
Adeolu Amole <amoleadeoluadex@gmail.com>
2026-03-02 14:03:46 +0100

    feat: Updates

--- app/components/Home/Features.tsx
+++ app/components/Home/Features.tsx
@@ -31,7 +31,10 @@ export default function Features({ mode }: FeaturesProps) {
   }
 
   return (
-    <section id="features" className="relative overflow-hidden w-full pb-20 md:py-10">
+    <section
+      id="features"
+      className="relative overflow-hidden w-full pb-20 md:py-10"
+    >
       <div className="absolute inset-0 w-full">
         <Image
           src={SectionBG}
